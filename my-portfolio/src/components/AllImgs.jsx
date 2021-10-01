ebf49201f3168441d5622a071294751a49a5ef2f
Gustavo Mendes <gusm217@gmail.com>
2021-10-01 20:07:21 -0300

    Finaliza pagina about

---
+++ my-portfolio/src/components/AllImgs.jsx
@@ -0,0 +1,29 @@
+import css from "../images/css.png";
+import bootstrap from "../images/bootstrap.png";
+import githubicon from "../images/git.png";
+import html from "../images/html.png";
+import js from "../images/js.png";
+import linux from "../images/linux.png";
+import mongo from "../images/mongo.png";
+import mysql from "../images/mysql.png";
+import node from "../images/node.png";
+import react from "../images/react.png";
+import redux from "../images/redux.png";
+import ouropreto from "../images/ouropreto.jpg";
+import express from "../images/express.png";
+
+export {
+  css,
+  bootstrap,
+  githubicon,
+  html,
+  js,
+  linux,
+  mongo,
+  mysql,
+  node,
+  react,
+  redux,
+  ouropreto,
+  express,
+};
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -28,8 +28,6 @@ export default function Home() {
               visit."
             </p>
           </section>
-          {/* colocar as stacks */}
-          {/* <section></section> */}
         </div>
         <Contact />
       </section>
